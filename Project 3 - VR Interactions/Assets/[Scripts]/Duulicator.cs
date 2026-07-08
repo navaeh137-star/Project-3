ccdb19c5c938d228b8784a349fe91539d350ff0c
navaeh137-star <navaeh137@gmail.com>
2026-07-08 13:12:20 -0700

    dahbeah

---
+++ Project 3 - VR Interactions/Assets/[Scripts]/Duulicator.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Duplcater : MonoBehaviour
+{ 
+    //public GameObject prefab1;
+      //  public Transform spawn;
+       // public GameObject prefab1;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Instantiate(prefab, spawn.position, spawn.rotation);
+    }
+}
